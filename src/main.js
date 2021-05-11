1e9c89df10850395f1f8dc54024f826b918b79d2
Alex Basinski <63376867+RockOfDoom@users.noreply.github.com>
2021-05-10 19:08:42 -0700

    changed dimensions of game and centered it

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 let config = {
     type: Phaser.AUTO,
     width: 640,
-    height: 480,
+    height: 750,
     scene: [Play],
 };
 
 let game = new Phaser.Game(config);
 
 let uiUnit = game.config.height / 15;
+let gridUnit = game.config.width / 51;
 
 let keySPACE;
 let textConfig;
