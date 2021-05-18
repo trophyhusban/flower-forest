b174eafb7ca964bb245a0371b3badbae996a514a
Ardent Reinhard <ardent@ucsc.edu>
2021-05-17 19:25:00 -0700

    changed test text (will add npcs)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -97,7 +97,7 @@ class Play extends Phaser.Scene {
             this.doppelganger.mirrorMode = !this.doppelganger.mirrorMode;
         });
 
-        this.openDialogue(uiUnit, 64, textJSON.example, textConfig);
+        this.openDialogue(uiUnit, 64, textJSON.puck1, textConfig);
 
         // this.dialogue = new DialogueBox(
         //     this,
